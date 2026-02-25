ec37eee03767355cfe2031bb298208d1f9626cba
saralove20 <saralove20@naver.com>
2026-02-25 10:02:30 +0900

    [Feat] 응답 코딩 컨벤션 적용

---
+++ src/main/java/com/example/demo/common/model/BaseResponseStatus.java
@@ -0,0 +1,26 @@
+package com.example.demo.common.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public enum BaseResponseStatus {
+    // 내가 원하는 에러를 다 만들어서 정리해둘 수 있음
+
+    // 2000번대 성공
+    SUCCESS(true, 2000, "요청이 성공했습니다"),
+
+    // 3000번대 클라이너트 입력 오류, 입력값 검증 오류
+    JWT_EXPIRED(false, 3001, "JWT 토큰이 만료되었습니다."),
+    JWT_INVALID(false, 3002, "JWT 토큰이 유효하지 않습니다."),
+    SIGNUP_DUPLICATE_EMAIL(false, 3003, "중복된 이메일입니다."),
+    SIGNUP_INVALID_PASSWORD(false, 3004, "비밀번호는 대,소문자, 숫자, 특수문자가 포함되어야 합니다."),
+
+    // 5000번대 실패
+    FAIL(false, 5000, "요청이 실패했습니다");
+
+    private final boolean success;
+    private final int code;
+    private final String message;
+}
