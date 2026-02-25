907157ef487635b5a61a6b7c909a1b128b458ef5
saralove20 <saralove20@naver.com>
2026-02-25 11:20:14 +0900

    [Feat] @RestControllerAdvice 로 전역 예외 처리

---
+++ src/main/java/com/example/demo/common/exception/GlobalExceptionHandler.java
@@ -0,0 +1,14 @@
+package com.example.demo.common.exception;
+
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+    @ExceptionHandler
+    public void handleException(Exception e) {
+        System.out.println("========= 예외 처리 =========");
+        System.out.println(e.getMessage());
+        System.out.println(("==========================="));
+    }
+}
