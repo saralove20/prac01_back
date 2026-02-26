e4838b852b58eb99b0b6135132e91f1108fcb212
saralove20 <saralove20@naver.com>
2026-02-26 19:32:49 +0900

    [Feat] 게시글과 이미지를 동시에 업로드
    컨트롤러의 매개변수에서 @RequestPart로 구분
    프론트에서 전송할 때 각각 part의 Content-Type 잘 구분해줘야한다.
    서비스 코드 짜줘야 함 (게시글이랑 묶이도록)

--- src/main/java/com/example/demo/upload/UploadController.java
+++ src/main/java/com/example/demo/upload/UploadController.java
@@ -1,10 +1,15 @@
 package com.example.demo.upload;
 
+import com.example.demo.board.BoardService;
+import com.example.demo.board.model.BoardDto;
 import com.example.demo.common.model.BaseResponse;
+import com.example.demo.user.model.AuthUserDetails;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -20,10 +25,23 @@ public class UploadController {
     // 컴포넌트 객체에 @Primary를 달아두면 해당 객체가 주입된다.
     // @Qualifier(value = "cloudUploadService")
     private final UploadService uploadService;
+    private final BoardService boardService;
 
     @PostMapping("/image")
     public ResponseEntity upload(List<MultipartFile> images) {
         List<String> result = uploadService.upload(images);
         return ResponseEntity.ok(BaseResponse.success(result));
+    }
+
+    @PostMapping("/imageWithDto")
+    public ResponseEntity upload(
+            @AuthenticationPrincipal AuthUserDetails user,
+            @RequestPart BoardDto.RegReq dto,
+            @RequestPart List<MultipartFile> images
+    ) {
+        System.out.println(dto.getTitle());
+        System.out.println(images.get(0).getOriginalFilename());
+
+        return ResponseEntity.ok("");
     }
 }
